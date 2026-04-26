dece57d0c2a5c1d0102c605aebfd2e283123a73d
Qusai-Alsalmi <qusai.alsalmi99@gmail.com>
2026-04-26 22:57:53 +0400

    feature: menus folder and login menu files

---
+++ src/menus/login_menu.cpp
@@ -0,0 +1,3 @@
+#include "login_menu.h"
+
+// This is a class for the login menu which will be used in the main function
---
+++ src/menus/login_menu.h
@@ -0,0 +1,13 @@
+#ifndef POS_SYSTEM_LOGIN_MENU_H
+#define POS_SYSTEM_LOGIN_MENU_H
+
+// This is a header for the login menu
+
+
+class login_menu {
+
+};
+
+
+
+#endif //POS_SYSTEM_LOGIN_MENU_H
