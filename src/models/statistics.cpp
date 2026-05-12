689906fe8efb0bc3a9837a3937d23f42e9c30972
Qusai-Alsalmi <qusai.alsalmi99@gmail.com>
2026-05-12 14:21:32 +0400

    feat: Added display func to stats

--- src/models/statistics.cpp
+++ src/models/statistics.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
-#include "../utils/data.h"
 #include "statistics.h"
+#include "../utils/data.h"
+#include "../utils/io.h" 
 
 Statistics::Statistics(std::string title, int numberOfSales) : title(title), numberOfSales(numberOfSales) {}
 
+void Statistics::display() const {
+
+    IO::print(Statistics::title);
+    IO::print(Statistics::numberOfSales);
+
+}
