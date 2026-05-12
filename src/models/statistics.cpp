8c81cd97b846c314c615033963168cc2ee404e58
Qusai-Alsalmi <qusai.alsalmi99@gmail.com>
2026-05-12 14:13:14 +0400

    feat: created a class for stats

---
+++ src/models/statistics.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "../utils/data.h"
+#include "statistics.h"
+
+Statistics::Statistics(std::string title, int numberOfSales) : title(title), numberOfSales(numberOfSales) {}
+
---
+++ src/models/statistics.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "../utils/data.h"
+
+class Statistics : public Record {
+    public:
+
+    const std::string title;
+    int numberOfSales; 
+
+    Statistics(std::string title, int numberOfSales);
+
+};
