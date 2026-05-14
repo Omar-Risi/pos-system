2b2befcc61091867a072e947094308d517f47701
Qusai-Alsalmi <qusai.alsalmi99@gmail.com>
2026-05-14 18:00:48 +0400

    fix: bugs in statistics.cpp

--- src/models/statistics.cpp
+++ src/models/statistics.cpp
@@ -3,11 +3,14 @@
 #include "../utils/data.h"
 #include "../utils/io.h" 
 
-Statistics::Statistics(std::string title, int numberOfSales) : title(title), numberOfSales(numberOfSales) {}
+Statistics::Statistics(std::string const title, int numberOfSales) : title(title), numberOfSales(numberOfSales) {}
+
+Statistics::~Statistics() {
+}
 
 void Statistics::display() const {
 
-    IO::print(Statistics::title);
-    IO::print(Statistics::numberOfSales);
+    IO::print(title);
+    IO::print(numberOfSales);
 
 }
