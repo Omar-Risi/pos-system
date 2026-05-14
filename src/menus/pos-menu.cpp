00ce9f6b162f341639ba518cb0020cc191b2f9bb
Faisalll78 <faisalmohd20060@gmail.com>
2026-05-14 13:59:01 +0400

    feat: added add product to cart

--- src/menus/pos-menu.cpp
+++ src/menus/pos-menu.cpp
@@ -1,10 +1,36 @@
 #include "pos-menu.h"
 #include "../utils/io.h"
+#include <vector>
+#include <string>
+#include "../utils/service.h"
+#include "../utils/data.h"
+#include "../models/product.h"
 
 PosMenu::PosMenu() : Menu("POS Menu") {
-    addOption("Add Product", []() {
+    Database* db = Service::getDatabase();
+    Table* productsTable = db->get("products");
+    std::vector<Product*> cart;
+
+    addOption("Add Product", [productsTable, &cart]() {
+
         std::string sku = IO::getString("Enter SKU:");
+        Product* product = dynamic_cast<Product*>(productsTable->get(sku));
+
+        if(product == nullptr) {
+            IO::print("Product not found");
+            return;
+        }
+
         int quantity = IO::getInt("Enter quantity:");
+        
+        if (product->stock < quantity) {
+            IO::print("Not enough stock");
+            return;
+        }
+
+        Product* cartItem = new Product(product->sku, product->name, product->price, quantity);
+        product->withdraw(quantity);
+        cart.push_back(cartItem);
     });
     addOption("Edit Cart", []() {
         IO::print("1. SKU: 1234 | Coffee | $2.50 | Qty: 2");
