60f20eb7f5ccba2fd7efc5ab931e3c3cfc04e935
Risi <reesiomar@gmail.com>
2026-05-21 09:10:30 +0400

    ref: renamed function name to open

--- src/menus/login-menu.cpp
+++ src/menus/login-menu.cpp
@@ -2,19 +2,20 @@
 #include "../utils/io.h"
 #include <string>
 
+bool LoginMenu::open(std::string user, std::string pass) {
 
-bool LoginMenu::show(std::string user, std::string pass) {
+  for (int i = 3; i > 0; i--) {
 
-    for (int i = 3; i > 0; i-- ) {
+    std::string userName = IO::getString("Enter username: ");
 
-        std::string userName = IO::getString("Enter username: ");
+    std::string password = IO::getString("Enter password: ");
 
-        std::string password = IO::getString("Enter password: ");
-
-        if (userName == user && password == pass) { return true; } else { continue; }
-
+    if (userName == user && password == pass) {
+      return true;
+    } else {
+      continue;
     }
+  }
 
-    return false;
-
-};
+  return false;
+}
